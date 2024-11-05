e16a055ae01222c63147c4bfb6f813a49803b66e
Reduan Masud <reduanmasudcse@gmail.com>
2024-11-05 13:57:42 +0600

    task added

---
+++ tests/task.spec.ts
@@ -0,0 +1,38 @@
+/**
+ * - [] Create New xCloud User Account
+ * - [] Test Errors In Account Creation Time
+ * - [] Check Contents in Registration Page
+ * - [] Handle Email Verification Process
+ * - [] Re-send Email Verification Process
+ * - [] Login Page Content Check
+ * - [] Login Page Error Check
+ * - [] Login Page Complete Login
+ * - [] {Initial} Dashboard Check
+ * - [] Contentfull Dashboard Check
+ * - [] Check Server Page without content
+ * - [] Check Server Page with content
+ * - [] Check Site Page Without Content
+ * - [] Check Site Page With Content
+ * - [] Create Nginx+MySql Server
+ * - [] Create Nginx+MariaDB Server
+ * - [] Create OLS+MySql Server
+ * - [] Create OLS+MariaDB Server
+ * ===============================
+ * === Server Check            ===
+ * ===============================
+ * - [] For {MySQL} check if everywhere is MySQL OK
+ * - [] For {MariaDB} check if everywhere is MariaDB OK
+ * - [] For {Nginx} check all Nginx Options are OK
+ * - [] For {OLS} check all OLS Options are OK
+ * - [] Check Empty Site List Page
+ * - [] Check Non-Empty Site List Page
+ * - [] Check if All Sidebar Menue is Visible
+ * - [] Check if All Top Menue is Visible
+ * --> Go All pages of SERVE and check content and other options are working or not.
+ * ===============================
+ * ===   Site Check            ===
+ * =============================== 
+ * - [] Check if All Sidebar Menue is Visible
+ * - [] Check if All Top Menue is Visible
+ * --> Go All pages of SERVE and check content and other options are working or not. 
+ */
