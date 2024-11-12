b37b363100c8aea6a60431d58a9d025c851577f7
Reduan Masud <reduanmasudcse@gmail.com>
2024-11-12 14:29:02 +0600

    ci implementing

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,13 +39,13 @@ export default defineConfig({
       },
       dependencies: ['setup'],
     },
-    // {
-    //   name: 'blog',
-    //   use: {
-    //     ...devices['Desktop Chrome'],
-    //     // baseURL: 'xcloud.host'
-    //   }
-    // }
+    {
+      name: 'blog',
+      use: {
+        ...devices['Desktop Chrome'],
+        // baseURL: 'xcloud.host'
+      }
+    }
 
     // Commented out other browsers
     // {
